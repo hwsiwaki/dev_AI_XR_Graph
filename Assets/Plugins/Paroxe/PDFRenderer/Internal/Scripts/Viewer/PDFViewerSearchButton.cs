04f77e0a1bb1445f6e1a884fd2b64f4747de2baa
iwaki <iwaki.yoshinori@headwaters.co.jp>
2025-12-10 11:49:23 +0900

    重いので複数回に分けてコミット

---
+++ Assets/Plugins/Paroxe/PDFRenderer/Internal/Scripts/Viewer/PDFViewerSearchButton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Paroxe.PdfRenderer.Internal.Viewer
+{
+
+    public class PDFViewerSearchButton : UIBehaviour
+    {
+        [SerializeField] PDFSearchPanel panel;
+
+        void Start()
+        {
+            panel.Close();
+        }
+        public void OnClick()
+        {
+#if !UNITY_WEBGL
+            panel.Toggle();
+#endif
+        }
+    }
+}
